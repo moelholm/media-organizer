43a12c4a9647548369290c781db8ab2769750caa
moelholm <moelholm@gmail.com>
2023-10-22 07:52:49 +0200

    initial iac commit

---
+++ iac/lib/iac-media-organizer-stack.ts
@@ -0,0 +1,49 @@
+import * as cdk from "aws-cdk-lib";
+import {Construct} from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import {BillingMode} from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
+
+export class IacMediaOrganizerStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        //
+        // Function: Runs the application
+        //
+        const lambdaFunction = this.createFunction(
+            "mediaorganizerfunction",
+            "build/announce-new-accounts-service.announceNewAccounts",
+            "../app-javabubble/lambda-function.zip"
+        );
+        const newAccountsRule = new events.Rule(this, "mediaorganizerfunctionrule", {
+            schedule: events.Schedule.cron({minute: "30", hour: "*/2"}),
+            targets: [new targets.LambdaFunction(lambdaFunction)],
+        });
+    }
+
+    createFunction(
+        id: string,
+        handler: string,
+        lambdaZipFile: string,
+    ) {
+        const lambdaFunction = new lambda.Function(this, id, {
+            runtime: lambda.Runtime.JAVA_11,
+            handler: handler,
+            timeout: cdk.Duration.minutes(15),
+            code: lambda.Code.fromAsset(lambdaZipFile),
+            environment: {},
+        });
+        lambdaFunction.addToRolePolicy(
+            new iam.PolicyStatement({
+                actions: ["ssm:GetParametersByPath"],
+                resources: [
+                    `arn:aws:ssm:${this.region}:${this.account}:parameter/APPLICATION/MEDIA-ORGANIZER`,
+                ],
+            })
+        );
+        return lambdaFunction;
+    }
+}
---
+++ iac/test/iac-javabubble.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MediaOrganizer from '../lib/iac-media-organizer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/iac-media-organizer-stack.ts
+test('TO DO', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MediaOrganizer.IacMediaOrganizerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::QueueNOT-Really', {
+//     VisibilityTimeout: 300
+//   });
+});
